6ed46018ed8cc94839b17ceadfaf3d31e6415d1a
yangeee <534650291@qq.com>
2019-02-27 13:24:11 +0800

    update

--- animation-resume/js/main.js
+++ animation-resume/js/main.js
@@ -1,5 +1,25 @@
-var text = 'html{color:red}'
+var result2 =`
+/* 我需要一张白纸 */
 
+#paper{
+    position: fixed;
+    right: 0;
+    height: 100%;
+    width: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 16px;
+    background: black;
+
+}
+#paper > .content{
+    background: white;
+    height: 100%;
+    width: 100%;
+    padding-left: 16px;
+}
+`
 
 var result = `/*
 *面试官你好，我是XXX
@@ -19,6 +39,11 @@ html{
 #code{ 
     border: 1px solid red;
     padding: 16px;
+    overflow: hidden;
+    position: fixed;
+    width: 50%;
+    height: 100%;
+    left: 0;
 }
 
 /* 我需要一点代码高亮 */
@@ -35,25 +60,79 @@ html{
 
 /* 加点3D效果 */
 #code{
-
+    transform: rotate(360deg);
 }
 
 /* 不玩了，我来介绍一下我自己 */
-/* 我需要一张白纸 */
 `
 
-var n = 0
-var id = setInterval(() => {
-    n+=1
-    code.innerHTML = result.substring(0,n)
-    code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages.css, 'css')
-    styleTag.innerHTML = result.substring(0,n)
-    if(n >= result.length){
-        window.clearInterval(id)
-        fn2()
-    }   
-},10)
+var md =`
+# 自我介绍
 
-function fn2(){
-    
+我叫杨艺
+1998 年 12 月
+HUT学校毕业
+自学前端1年
+希望xxxxxxxxxxxxxx
+
+# 技能介绍
+
+熟悉 Javascript CSS
+
+# 项目介绍
+
+1. 轮播
+2. 画板
+3. 导航
+
+`
+function writeCode(prefix,code,fn){
+    let n = 0
+    let domCode =document.querySelector('#code')
+    domCode.innerHTML = prefix || ''
+    let id = setInterval(() => {
+        n += 1
+        domCode.innerHTML = Prism.highlight(prefix+code.substring(0,n), Prism.languages.css, 'css')
+        styleTag.innerHTML = prefix + code.substring(0, n)
+        domCode.scrollTop = domCode.scrollHeight
+        console.log(domCode.scrollTop)
+        if (n >= code.length) {
+            window.clearInterval(id)
+            fn.call()
+        }
+    },30)
 }
+
+function writeMarkdown(markdown){
+    let domPaper = document.querySelector('#paper>.content')
+    let n = 0
+    let id = setInterval(() => {
+        n += 1
+        domPaper.innerHTML = markdown.substring(0,n)
+        domPaper.scrollTop = domPaper.scrollHeight
+        if (n >= markdown.length) {
+            window.clearInterval(id)
+        }
+    },30)
+}
+
+function createPaper(fn) {
+    var paper = document.createElement('div')
+    paper.id = 'paper'
+    var content = document.createElement('pre')
+    content.className = 'content'
+    document.body.appendChild(paper)
+    paper.appendChild(content)
+    fn.call()
+}
+writeCode('',result, ()=>{
+    createPaper(()=>{
+        writeCode(result,result2,()=>{
+            writeMarkdown(md)
+        })
+    })
+})
+
+
+
+
