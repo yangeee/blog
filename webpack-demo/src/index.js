26a39fb01447164711633d812ed92c6deb4583fc
yangeee <534650291@qq.com>
2019-03-07 18:10:23 +0800

    try  Arrow functions

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import './style.scss';
 
-function component() {
+var component = () => {
     let element = document.createElement('div');
   
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
@@ -11,4 +11,5 @@ function component() {
   }
   
   document.body.appendChild(component());
+  
   
